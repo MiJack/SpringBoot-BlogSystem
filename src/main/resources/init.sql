5a69d4bd3ad4ea46cddc48217a10c2dc7497ff72
Mi&Jack <893380824@qq.com>
2017-10-19 23:03:36 +0800

    引入editor.md

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -2,9 +2,9 @@ INSERT INTO role (id, name,display_name) VALUES (1, 'ROLE_ROOT','超级管理员
 INSERT INTO role (id, name,display_name) VALUES (2, 'ROLE_ADMIN','管理员');
 INSERT INTO role (id, name,display_name) VALUES (3, 'ROLE_USER','用户');
 #
-INSERT INTO user (id, username, password, email,avatar,hot_value,update_time) VALUES (1, 'root', '123456', 'root@mijack.cn','http://localhost:8080/favicon.ico',0,'2017-09-26 00:00:00');
-INSERT INTO user (id, username, password, email,avatar,hot_value,update_time) VALUES (2, 'admin', '123456', 'admin@mijack.cn','http://localhost:8080/favicon.ico',0,'2017-09-26 00:00:00');
-INSERT INTO user (id, username, password, email,avatar,hot_value,update_time) VALUES (3, 'mijack', '123456', 'mijack@mijack.cn','http://localhost:8080/favicon.ico',0,'2017-09-26 00:00:00');
+INSERT INTO uploader (id, username, password, email,avatar,hot_value,update_time) VALUES (1, 'root', '123456', 'root@mijack.cn','http://localhost:8080/favicon.ico',0,'2017-09-26 00:00:00');
+INSERT INTO uploader (id, username, password, email,avatar,hot_value,update_time) VALUES (2, 'admin', '123456', 'admin@mijack.cn','http://localhost:8080/favicon.ico',0,'2017-09-26 00:00:00');
+INSERT INTO uploader (id, username, password, email,avatar,hot_value,update_time) VALUES (3, 'mijack', '123456', 'mijack@mijack.cn','http://localhost:8080/favicon.ico',0,'2017-09-26 00:00:00');
 #
 INSERT INTO user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO user_role (user_id, role_id) VALUES (2, 2);
